64b036dd27a83271866c1e7f5065c2ae9d43616b
dilu583 <dilu583@gmail.com>
2016-04-10 20:59:04 -0500

    Added files via upload

--- Driver.cpp
+++ Driver.cpp
@@ -1,21 +1,41 @@
 #include "Driver.h"
 
-vector<vector<bool>> readOccupation(){
-	vector <vector<bool>> b;
-	return b;
-}
 
-void waitForChange(int timeOut){
-	// blocks until something changes on the board
+void Driver::write_color_table(int i,int j,lpd8806_buffer buf,RGB color)
+ {
+	  if(j%2==0)
+	  {
 	
+		  write_gamma_color(&buf.pixels[j*8+i],color.r,color.g,color.b);
+	  }
+	  else if (j%2!=0)
+		  
+	  {
+		  
+		  write_gamma_color(&buf.pixels[(j+1)*8-1-i],color.r,color.g,color.b);
+	  }
+  }
+  
+  
+
+void Driver::writeToLeds(RGB rgbp[8][8],lpd8806_buffer buf, RGB color)
+{
+	for(int i=0; i<8; i++)
+	{
+		for(int j=0; j<8;j++)
+		{
+			RGB color = rgbp[i][j];
+			write_color_table(i,j, buf, color);
+			
+		}
+}
 }
 
-void writeToLeds(vector<vector<RGB>> lightPositions){
-	
+void Driver::clear_led(lpd8806_buffer buf,const int leds)
+{
+	for(int i=0;i<leds;i++) {
+    
+      write_gamma_color(&buf.pixels[i],0x00,0x00,0x00);
+      
+  }
 }
-
-vector <vector<bool>> occupiedBoard(){
-	vector <vector<bool>> b;
-	return b;
-}
-
--- Driver.h
+++ Driver.h
@@ -1,22 +1,91 @@
 #include <cstdlib>
 #include <vector>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <ctime>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <cassert>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/select.h>
+
+
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <time.h>
+
+
+#include <ctime>
+#include <fstream>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "lpd8806led.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <wiringPi.h>
+#include <pcf8574.h>
 
 using namespace std;
 
 typedef struct RGB {
-	unsigned byte r;
-	unsigned byte g;
-	unsigned byte b;
+	int r;
+	int g;
+	int b;
 } RGB;
+
+
+
+
+
+
 
 class Driver{
 	
 	public:
+		void writeToLeds(RGB rgbp[8][8],lpd8806_buffer buf,RGB color);
 		
-		vector<vector<bool>> readOccupation(); 
-		void waitForChange(int timeOutMS);
-		void writeToLeds(vector<vector<RGB>> lightPositions);
+		void write_color_table(int i,int j,lpd8806_buffer buf, RGB color);
 		
-		vector<vector<bool>> occupiedBoard();
+		void clear_led(lpd8806_buffer buf,const int leds);
+		//vector<vector<bool>> readOccupation(); 
+	//	void waitForChange(int timeOutMS);
+		//void writeToLeds(RGB rgbp[8][8]);
+		
+		//vector<vector<bool>> occupiedBoard();
 
-}
+};
