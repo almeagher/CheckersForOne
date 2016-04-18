79be4575cc8f8e648ba60552b9d9ede692ce6423
Allison Meagher <almeagher1@gmail.com>
2016-04-18 13:16:28 -0500

    update test

--- Test.cpp
+++ Test.cpp
@@ -5,14 +5,17 @@
 
 using namespace std;
 
-vector <vector<RGB>> lightBoard(vector<vector<int>> board){
+RGB** lightBoard(vector<vector<int>> board){
 	
 	
-	vector<vector<RGB>> lightBoard;
+	RGB** lightBoard;
+	lightBoard = new RGB*[8];
+	
 	RGB black = { 0, 0, 0}
 	RGB green = { 0, 255, 0}
-	for(int r = 0; r < board.size(); r++){
-		for(int c = 0; c < board.size(); c++){
+	
+	for(unsigned r = 0; r < board.size(); r++){
+		for(unsigned c = 0; c < board.size(); c++){
 			if(board[r][c] == 1)
 				lightBoard[r][c] = green;
 			else
@@ -24,7 +27,10 @@ vector <vector<RGB>> lightBoard(vector<vector<int>> board){
 
 int main(int argc, char *argv[]) {
 	Driver driver;
-	vector<vector<int>> reedBoard = driver.getPositions();
+	while(1){
+		int** reedBoard = driver.getPositions();
+		driver.writeToLeds(lightBoard(reedBoard));
+		
+	}
 	
-	driver.writeToLeds(lightBoard(reedBoard));
 }
