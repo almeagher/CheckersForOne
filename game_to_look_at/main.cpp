d550e4f5aea8955cc9fb5e32820c34355d88fd96
almeagher <almeagher@users.noreply.github.com>
2016-05-04 12:26:30 -0500

    checkers as of final demo

--- game_to_look_at/main.cpp
+++ game_to_look_at/main.cpp
@@ -15,19 +15,35 @@ void endEarly(bool endEarly){
 }
 
 int main(int argc, char** argv) {
+	cout<<"start\n";
+	wiringPiSetupGpio();
+	pinMode(16,INPUT);
+	pullUpDnControl(16, PUD_UP);
+	
+	pinMode(19,INPUT);
+	pullUpDnControl(19, PUD_UP);
+	
+	int p1= digitalRead(19);
+	int p2= digitalRead(16);
 	
 	Driver driver = Driver();
 	driver.scan();
 	Checkerboard cb = Checkerboard(driver.table);
-	//driver.printint();
 	
 	RGB ledBoard[8][8];
-	//driver.clear_led();
+	driver.clear_led();
 	while(cb.canStart(driver) == false){
 		
 	}
-	bool player1 = driver.isPlayer1Comp();
-	bool player2 = driver.isPlayer2Comp();
+	
+	
+	bool player1 = false;
+	bool player2 = false;
+	if(p1 == 1) player1=true;
+	if(p2 == 1) player2=true;
+	
+	cout << "P1: " << player1 << endl;
+	cout << "P2: " << player2 << endl;
 	game checkers;
 	checkers.playTheGame(player1, player2, driver, cb);
 	
